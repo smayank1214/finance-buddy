e5eec6be963f99646ff72b7588f5230a114a570b
smayank1214 <rawatmayank716@gmail.com>
2024-07-05 07:28:29 +0530

    Navbar and Theme setup

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,8 @@ import React from "react";
 function Logo() {
   return (
     <a href="/" className="flex items-center gap-2">
-      <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+      <PiggyBank className="stroke h-11 w-11 stroke-blue-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
         FinanceBuddy
       </p>
     </a>
@@ -15,8 +15,8 @@ function Logo() {
 export function LogoMobile() {
   return (
     <a href="/" className="flex items-center gap-2">
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
-        BudgetTracker
+      <p className="bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        FinanceBuddy
       </p>
     </a>
   );
